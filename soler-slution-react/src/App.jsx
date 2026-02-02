69f0097975ae4862a693153f4a81caab26d1d98a
Mahmood Hassan Rameem <rameem2019@gmail.com>
2026-02-02 22:19:13 +0600

    banner navbar

--- soler-slution-react/src/App.jsx
+++ soler-slution-react/src/App.jsx
@@ -1,9 +1,11 @@
+import Banner from "./components/screens/Home/Banner";
 import Navigation from "./components/shared/Navigation";
 
 const App = () => {
   return (
     <div>
       <Navigation />
+      <Banner />
     </div>
   );
 };
--- soler-slution-react/src/components/common/LogoComponent.jsx
+++ soler-slution-react/src/components/common/LogoComponent.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Logo from "./Logo";
 import { Menu } from "lucide-react";
 import MobileSideBar from "./MobileSideBar";
 
 const LogoComponent = () => {
   const [toggle, setToggle] = useState(false);
+
+  const menuRef = useRef();
+
+  // console.log(menuRef);
+
+  useEffect(() => {
+    // console.log(menuRef.current);
+    // menuRef.current.style.backgroundColor = "red";
+
+    document.addEventListener("click", (e) => {
+      menuRef.current.contains(e.target) ? setToggle(true) : setToggle(false);
+    });
+  }, []);
+
   return (
     <div className=" w-full lg:w-auto flex items-center justify-between">
       <Logo />
-      <button onClick={() => setToggle(true)}>
+      <button ref={menuRef} onClick={() => setToggle(true)}>
         <Menu className=" lg:hidden text-white" />
       </button>
       <MobileSideBar toggle={toggle} setToggle={setToggle} />
